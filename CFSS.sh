22f52b903696baef0fa6dd1faef56a3a6844566f
Juan Jose <jjsa96@gmail.com>
2019-02-07 14:02:07 -0600

    Actualización del archivo.

--- CFSS.sh
+++ CFSS.sh
@@ -7,11 +7,13 @@ if [ -d "$DIRECTORY" ]; then
 	tar -cvzf $DIRECTORY.tar.gz $DIRECTORY
 	DIRECTORY=$1.tar.gz
 fi
-
+# Firma GPG del archivo, haciendo constar que la persona que lo firma es quien lo envía.
 gpg --sign --armor $DIRECTORY
 
+# Cifrado del archivo.
 openssl ts -query -data $DIRECTORY -cert -sha256 -no_nonce -out request.tsq
 
+# Sellado temporal provisto por el servicio de SINPE.
 cat request.tsq | curl -s -S -H 'Content-Type: application/timestamp-query' --data-binary @- http://tsa.sinpe.fi.cr/tsaHttp/ -o $DIRECTORY.tsr
 
 rm -f request.tsq
